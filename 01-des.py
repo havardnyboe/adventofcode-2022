9704111ec04de6ec9e05adf50e35374e096b1b86
havardnyboe <havard.nyboe@gmail.com>
2022-12-02 10:02:19 +0100

    clean up

--- 01-des.py
+++ 01-des.py
@@ -1,36 +1,30 @@
 # --- Day 1: Calorie Counting ---
-# ikke vakkert men det funker :P
 
 def main():
 
-  calories: list = []
-  top_elf: int = 0
-  top_three: int = 0
+    calories = []
+    top_elf = 0
+    top_three = 0
 
-  with open("01-des.txt", "r") as file:
-    elves = file.read().split("\n\n")
-    for i in range(len(elves)):
-      try:
-        elves[i] = [int(cal) for cal in elves[i].split("\n")]
-      except ValueError:
-        pass
+    with open("01-des.txt", "r") as file:
+        elves = file.read().split("\n\n")
+        for i in range(len(elves)):
+            elves[i] = [int(cal) for cal in elves[i].split("\n")]
 
-    for elf in elves:
-      try:
-        calories.append(sum(elf))
-      except TypeError:
-        pass
+        for elf in elves:
+            calories.append(sum(elf))
 
-    if not top_elf:
-        top_elf = max(calories)
-    for i in range(3):
-      top_three += max(calories)
-      calories.pop(calories.index(max(calories)))
+        if not top_elf:
+            top_elf = max(calories)
+        for i in range(3):
+            top_three += max(calories)
+            calories.pop(calories.index(max(calories)))
 
-    print(f"""
+        print(f"""
     Top Elf: \t {top_elf}
     Top Three: \t {top_three}
     """)
 
+
 if __name__ == "__main__":
-  main()
+    main()
